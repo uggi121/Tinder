35e89754391e459e15c1f67f72efb6144055ed7a
sidharth3 <sidhu.beena@gmail.com>
2019-10-12 22:08:46 +0800

    inc. pyshell

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var myPythonScriptPath = './script.py';
+  const { PythonShell } = require("python-shell");
+  var pyshell = new PythonShell(myPythonScriptPath);
+  pyshell.send('hello world')
+  pyshell.on('message', function(message) 
+  {
+    console.log(message);
+  })
+
   res.render('index', { title: 'Express' });
+  pyshell.end(function(err){
+    if(err){
+      throw err;
+    };
+  console.log('finished');
+  })
 });
+
+
 router.post('/')
 
 module.exports = router;
